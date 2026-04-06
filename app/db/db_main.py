94d70f804729bb2dc50fae35d334b970ebb062a2
platohaDebil <zhitnikov05@inbox.ru>
2026-04-06 18:28:03 +0300

    quick fix2

--- app/db/db_main.py
+++ app/db/db_main.py
@@ -1,9 +1,9 @@
-from db_manager import create_db, createTables
+from app.db.db_manager import create_db, createTables
 
 
-def main():
+def db_init():
     create_db()
     createTables()
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
