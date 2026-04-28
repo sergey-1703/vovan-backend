2d4509b16f5ed0fc9475e5b8813a449c94626c5c
Sergey <161504480+sergey-1703@users.noreply.github.com>
2026-04-28 17:41:37 +0300

    feat: root endpoint

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from fastapi.responses import RedirectResponse
+from fastapi.security import HTTPAuthorizationCredentials
+from app.security.token_manager import get_id_by_token
 from app.routers.auth import auth_manager
+from app.tools.config import security
 from app.routers.api import base_api
 from app.routers.users import user_manager
-from app.db.db_main import db_init, add_test_users
+from app.db.db_main import db_init, add_test_users, get_user_by_id
 import uvicorn
 
 app = FastAPI()
@@ -11,8 +15,14 @@ app.include_router(base_api.router)
 app.include_router(user_manager.router)
 
 @app.get("/")
-def root():
-    return "null"
+def root(token: HTTPAuthorizationCredentials = Depends(security)):
+    if not token:
+        return RedirectResponse("/login")
+    current_user_id = get_id_by_token(token.credentials)
+    user = get_user_by_id(current_user_id)
+    if not user:
+        return RedirectResponse("/login")
+    return RedirectResponse("/chats")
 
 if __name__ == "__main__":
     db_init()
